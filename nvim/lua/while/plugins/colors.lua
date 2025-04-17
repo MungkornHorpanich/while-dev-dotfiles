ba8f99f6287681975d9a48da97d859ca14c14e39
M <MungkornHorpanich@gmail.com>
2025-04-17 13:04:32 +0700

    changing the colors'

--- nvim/lua/while/plugins/colors.lua
+++ nvim/lua/while/plugins/colors.lua
@@ -1,41 +1,41 @@
 -- Making transparent
--- function ColorMyPencils(color)
---    color = color or "onedark"
---    vim.cmd.colorscheme(color)
---    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
--- end
+function ColorMyPencils(color)
+   color = color or "retrobox"
+   vim.cmd.colorscheme(color)
+   vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+end
 --
 -- so this is the best way to config neovim for beginner
-
 return {
-    {
-        "folke/tokyonight.nvim",
-    },
-    {
-        "rose-pine/neovim",
-        name = "rose-pine",
-        config = function()
-            require("rose-pine").setup({
-                disable_background = true,
-            })
+   {
+      "folke/tokyonight.nvim",
+   },
+   {
+      "rose-pine/neovim",
+      name = "rose-pine",
+      config = function()
+         require("rose-pine").setup({
+            disable_background = true,
+         })
 
-            -- vim.cmd("colorscheme rose-pine")
+         -- vim.cmd("colorscheme rose-pine")
 
-            -- ColorMyPencils()
-        end,
-    },
-    {
-        "EdenEast/nightfox.nvim",
-        priority = 1000,
-        config = function()
-            require("nightfox").setup({
-                options = {
-                    transparent = true, -- Disable setting background
-                    terminal_colors = true, -- Set terminal colors (vim.g.terminal_color_*) used in `:terminal`
-                },
-            })
+         -- ColorMyPencils()
+      end,
+   },
+   {
+      "EdenEast/nightfox.nvim",
+      priority = 1000,
+      config = function()
+         require("nightfox").setup({
+            options = {
+               transparent = true, -- Disable setting background
+               terminal_colors = true, -- Set terminal colors (vim.g.terminal_color_*) used in `:terminal`
+            },
+         })
 
-            vim.cmd("colorscheme nightfox")
-        end,
-    },
+         -- vim.cmd("colorscheme nightfox")
+      end,
+   },
+   ColorMyPencils(),
 }
